ca6f48711b5b993ab75a18daf7e6a0246f0abdef
Kamilly Medino <148277123+kamillyvm@users.noreply.github.com>
2025-04-14 22:53:19 -0300

    Update primeiroAlgoritmo.m

--- primeiroAlgoritmo.m
+++ primeiroAlgoritmo.m
@@ -2,7 +2,7 @@ close all
 clear all
 
 %ler imagem de um arquivo
-im = imread('C:\Users\vanes\OneDrive\Documentos\PDI\grao.jpeg');
+im = imread('grao.jpeg');
 
 %converter uma imagem colorida (RGB) em tons de cinza
 imGray = rgb2gray(im);
