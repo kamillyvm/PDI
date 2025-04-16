efbe56dfc50dd48977a885fa9f4a3ced4c441e77
Kamilly Medino <148277123+kamillyvm@users.noreply.github.com>
2025-04-15 23:30:32 -0300

    Update objetivoA.m

--- Pratica 1/objetivoA.m
+++ Pratica 1/objetivoA.m
@@ -1,16 +1,20 @@
 close all
 clear all
 
+%ler a imagem "círculo.png" e armazenar na matriz m1
 m1 = imread('circulo.png');
 
+%mostrar a matriz m1 em uma nova janela
 figure('Name', 'm1')
 imshow(m1)
 
+%criar uma nova matriz m2 copiando a matriz m1
 m2 = uint8(m1);
 
 figure('Name', 'm2')
 imshow(m2)
 
+%percorrer a matriz m2 e definir os valores dos pixels do círculo como cinza médio
 for(i=1:size(m2,1))
   for(j=1:size(m2,2))
     if(m2(i,j))
@@ -24,4 +28,5 @@ endfor
 figure('Name', 'circulo modificado')
 imshow(m2)
 
+%salvar a matriz m2 no disco com o nome "circulo2.png"
 imwrite(m2, 'circulo2.png')
